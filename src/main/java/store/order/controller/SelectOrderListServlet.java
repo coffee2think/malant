f9e90ed79becdb49fae97cf1166be117e2c8c6d9
Hong <hsc890125@gmail.com>
2023-09-05 23:54:19 +0900

    conflict solving2

--- src/main/java/store/order/controller/SelectOrderListServlet.java
+++ src/main/java/store/order/controller/SelectOrderListServlet.java
@@ -1,11 +1,17 @@
-package store.orderList.controller;
+package store.order.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import store.order.model.service.OrderService;
+import store.order.model.vo.ProductOrder;
 
 /**
  * Servlet implementation class SelectOrderListServlet
@@ -27,15 +33,32 @@ public class SelectOrderListServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		
+		
+		String userno = request.getParameter("userNo");
+		
+		System.out.println("olist 들어온 유저 : " + userno);
+		
+		ArrayList<ProductOrder> olist = new OrderService().selectOrderlist(userno);
 
+		
+		RequestDispatcher view = null;
+		if (olist != null) {
+			view = request.getRequestDispatcher("views/store/order/myOrderList.jsp");
+			request.setAttribute("olist", olist);
+			System.out.println("servlet성공 : " + olist.toString());
+		} else {
+			System.out.println("servlet실패 : ");
+			view = request.getRequestDispatcher("views/common/error.jsp");
+			request.setAttribute("message", "조회된 상품이 없습니다.");
+		}
+		view.forward(request, response);
+	}
 }
